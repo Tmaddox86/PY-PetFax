9dcd9dc36687a91bf976651011399d0f61180684
Tara Maddox <TMaddox85@gmail.com>
2023-06-01 22:08:32 -0700

    tables are showing in db

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -4,7 +4,7 @@ from flask_migrate import Migrate
 def create_app():  
     app = Flask(__name__)
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Lovingtech@localhost:5000/petfax'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:lovingtech@localhost:5432/petfax'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False             
 
     from . import models
